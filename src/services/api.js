eae1a5ad17a8a5c4df8904bf75aeb0cbdbdfa7fc
VivianaEscobarB <avescobarb@uqvirtual.edu.co>
2025-04-06 19:40:54 -0500

    up

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dental-management-system-backend.onrender.com/api",
+  baseURL: "http://localhost:8081/api",
   headers: { "Content-Type": "application/json" }
 });
 
